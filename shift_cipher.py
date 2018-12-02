313d47821d2ad0c7a4d96dd0040c6fcd68ab0842
Martin Cuellar <nelbren@unitec.edu>
2018-12-01 19:01:07 -0600

    Update shift_cipher.py for MicroPython compatibility

--- shift_cipher.py
+++ shift_cipher.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
-# shift_cipher_v0.0.1.py
+# shift_cipher_v0.0.2.py
 # nelbren@nelbren.com
-# UsePythonIn10lines
+# FormatFix4MicroPython
 def shift_str(x,d):
-  r=''    
+  r=''
   for c in x:
     o = ord(c)
     r += chr(o if o == 32 else o + d - 26 if o + d > 90 else o + d)
   return r
 def cipher2text(x):
   for d in range(0, 25):
-    print("intento " + format(d, '02') + ": " + shift_str(x,d))  
+    print("intento " + '{:02}'.format(d) + ": " + shift_str(x,d))
 cipher2text('PHHW PH DIWHU WKH WRJD SDUWB')
